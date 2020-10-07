5a10fa1daed22a854c9ee5bf2d94e78c9a53ed23
Daniel McDonald <dajmcdon@gmail.com>
2020-10-06 21:07:46 -0700

    fix template package, add gh-actions

--- forecasters/template/R/template-package.R
+++ forecasters/template/R/template-package.R
@@ -2,8 +2,9 @@
 #'
 #' @name template-package
 #' @docType package
-#' @author first_name last_name
-#' Maintainer: first_name last_name<email>
+#' @description The template forecaster
+#' @author firstname lastname
+#' Maintainer: first_name last_name email
 #' @keywords package
 NULL
 
--- forecasters/template/README.md
+++ forecasters/template/README.md
@@ -1,3 +1,7 @@
 # template
 
+<!-- badges: start -->
+  [![R build status](https://github.com/cmu-delphi/covid-19-forecast/workflows/R-CMD-check/badge.svg)](https://github.com/cmu-delphi/covid-19-forecast/actions)
+  <!-- badges: end -->
+
 The template forecaster
