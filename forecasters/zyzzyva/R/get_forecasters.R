6e1a9660976dab02f3ede85887debd876779bfc8
sgsmob <sgsmob@users.noreply.github.com>
2021-01-07 11:07:12 -0500

    Add evalcast-compliant variant of zyzzyva (#30)
    * add codeowners
    
    * initial copy of zyzzyva to this repo
    
    * forecaster function has evalcast-style arguments
    
    * start of refactor to evalcast compliance
    
    * fix geo typo
    
    * remove legacy issue hacks now that every row has an issue
    
    * adapt preprocessing to work with evalcast
    
    * 'test' for running evalcast version
    
    * change `location` to `geo_value` in modeling code
    
    * test script updated to use county cases
    
    * remove evalforecast dependency
    
    * remove dependency on evalforecast
    
    * 'R-package' -> 'R-packages'
    
    * update documentation
    
    Co-authored-by: Daniel McDonald <dajmcdon@gmail.com>

---
+++ forecasters/zyzzyva/R/get_forecasters.R
@@ -0,0 +1,85 @@
+#' @include main.R
+NULL
+
+#' Get the list of forecasters and associated forecaster types
+#' provided by this package.
+#'
+#' @description The evaluator will first call this function with the
+#'     parameters shown below to determine all the forecasters
+#'     available. It expects to get back a named list of lists of
+#'     forecasting functions and types. If a forecasting function is
+#'     not available for a given set of parameters, an `NA` should
+#'     returned _instead of_ a function. This tells the evaluator to
+#'     ignore that forecaster in a run: it ignores anything that is
+#'     not a function. See examples in code below.
+#'
+#' @param response the response (e.g. "usa-facts_deaths_incidence_num")
+#' @param incidence_period the incidence period (e.g. "epiweek" for
+#'     now, for all forecasters)
+#' @param ahead the ahead parameter (e.g. 1, 2, 3, 4)
+#' @param forecast_date the date of the forecast
+#' @param geo_type the geographic type (e.g "county" or "state" or
+#'     "hrr" or "msa"... but for now only the first two),
+#' @param n_locations the number of locations (for now we will use 200
+#'     for this)
+#' @return a named list, with each element of the list consisting of a
+#'     forecaster function and type (one of `c("standalone",
+#'     "ensemble")`). Unavailable forecasters are marked as
+#'     `list(forecaster = NA, type = "standalone")`.
+#' @export get_forecasters
+get_forecasters  <- function(response,
+                             geo_type,
+                             n_locations=200) {
+
+    covidcast_cluster_covariates = list(
+        ds.covariate("usa-facts_confirmed_incidence_num",
+                     tr = tr.log_pad, lags = c(1, 2, seq(3,21,3)), do_rollsum = T)
+    )
+
+    covidcast_model_covariates = list(
+        ds.covariate("usa-facts_confirmed_incidence_num", tr = tr.log_pad,
+                     lags = c(1, 2, seq(3,21,3)), do_rollsum = T),
+        ds.covariate("fb-survey_smoothed_hh_cmnty_cli",
+                     lags = seq(3,28,7), do_rollsum = T),
+        ds.covariate("indicator-combination_nmf_day_doc_fbc_fbs_ght",
+                     lags = seq(3,28,7), do_rollsum = T)
+    )
+
+    modeling_options <- list(
+        cluster_covariates = covidcast_cluster_covariates,
+        cdc_probs = c(0.01, 0.025, seq(0.05, 0.95, by = 0.05), 0.975, 0.99),
+        model_covariates = covidcast_model_covariates,
+        log_response = TRUE,
+        location_pcs = 0,
+        n_clusters = 1,
+        learner = "stratified_linear",
+        use_cv_lasso = FALSE,
+        use_median_point = TRUE,
+        add_interactions = FALSE,
+        impute_last_3_response_covariate = TRUE,
+        seed = 2020,
+        weeks_back = 4,
+        response = response
+    )
+
+    ## Currently we only work with "county" or "state" level forecasts
+
+    ## If your function has not been completed for geo_type == "state"
+    ## say, you can return NA as the "else" part shows below.  Note
+    ## also, how you can pass along additional parameters besides the
+    ## six mandatory ones.
+
+    if (geo_type %in% c("county", "state")) {
+        list(
+            zyzzyva_covidcast =
+                list(forecaster=stacked_forecaster(
+                         n_locations=nlocations,
+                         modeling_options=modeling_options),
+                     type="standalone")
+        )
+    } else {
+        list(zyzzyva =
+                 list(forecaster=NA, type="standalone")
+             )
+    }
+}
---
+++ forecasters/zyzzyva/R/modeling.R
@@ -0,0 +1,297 @@
+#' @include calibration.R transformation.R
+NULL
+
+#' Fit a learner to the training data
+#'
+#' @param train_test list of training and testing data
+#' @param modeling_options a named list, additional elements of which
+#'     overrides learner-dependent options set within the code
+ml.fit_model <- function(train_test,
+                         modeling_options) {
+  if (modeling_options$learner == "stratified_linear") {
+    ml.stratified_linear(train_test, modeling_options)
+  } else {
+    stop("available learners are ('stratified_linear')")
+  }
+}
+
+#' Finds the stratified clusters. 
+#'
+#' @param test_X covariates as of forecast_date
+#' @param modeling_options a named list, additional elements of which
+#'     overrides learner-dependent options set within the code
+#' @importFrom stats quantile
+ml.get_clusters <- function(test_X,
+                            modeling_options) {
+
+  # get variable names for clustering
+  cluster_vars <- c()
+  for (var in modeling_options$cluster_covariates) {
+    name <- var$name
+    for (lag_num in var$lags) {
+      if (lag_num > 0) {
+        temp_name <- paste(name, LAG_SUFFIX, 
+        stringr::str_pad(lag_num, 2, pad="0"), sep="_")
+        if (var$do_rollavg) {
+          temp_name <- paste(temp_name, ROLLAVG_SUFFIX, sep="_")
+        } else if (var$do_rollsum) {
+          temp_name <- paste(temp_name, ROLLSUM_SUFFIX, sep="_")
+        }
+        cluster_vars <- c(cluster_vars, temp_name)
+      } else {
+        if (var$do_rollsum) {
+          cluster_vars <- c(cluster_vars, paste(name, ROLLSUM_SUFFIX, sep = "_"))
+        } else if (var$do_rollavg) {
+          cluster_vars <- c(cluster_vars, paste(name, ROLLAVG_SUFFIX, sep = "_"))
+        } else {
+          cluster_vars <- c(cluster_vars, name)
+        }
+      }
+    }
+  }
+    
+  # perform simple equal-size clustering based off cases
+  n_clusters <- modeling_options$n_clusters
+  cluster_cols_idx <- which(colnames(test_X) %in% cluster_vars)
+  cluster_vars <- test_X[, cluster_cols_idx]
+  sum_cols <- rowSums(cluster_vars)
+  clusters <- rep(1, length(sum_cols))
+  cluster_quantiles <- stats::quantile(sum_cols, c(1:(n_clusters - 1)) / n_clusters)
+  for (i in 1:(n_clusters - 1)) {
+    clusters[sum_cols>cluster_quantiles[i]] <- i + 1
+  }
+  clusters
+}
+
+#' Fit a simple linear model within stratified clusters.
+#'
+#' Clusters are determined using a simple scheme: order the locations
+#' by the number of cases. Sort, then divide into k equal sized groups. 
+#' Within each cluster, the following model is fit:
+#'    cases ~ cases_lag1 + fb_lag1 + combined_ind_lag1 + log(pop) +
+#'            slope(cases) + slope(fb) + slope(combined_ind).
+#' The slope is computed using least squares over past lags of each signal,
+#' for each location individually. If in modeling_options, interactions 
+#' are specified, all cases_lag1 interactions will be added.
+#' Calibration is not currently performed.
+#' 
+#' @param train_test list of training and testing data
+#' @param modeling_options a named list, additional elements of which
+#'     overrides learner-dependent options set within the code
+#' @importFrom logger log_debug
+#' @importFrom Iso pava
+#' @importFrom stats lsfit
+#' @importFrom quantgen quantile_lasso
+#' @importFrom glmnet glmnet
+ml.stratified_linear <- function(train_test,
+                                 modeling_options) {
+  # extract items from train_test
+  train_X <- train_test$train_X
+  train_y <- train_test$train_y
+  test_X <- train_test$test_X
+  train_row_locations <- train_test$train_row_locations[[1]]
+  test_row_locations <- train_test$test_row_locations[[1]]
+  n <- nrow(train_X)
+  
+  # get clusters
+  clusters <- ml.get_clusters(test_X, modeling_options)
+  cluster_locs <- data.frame(cluster_id=clusters,
+                             location=train_test$test_row_locations)
+  
+  # create new_train_X, new_test_X with slope variables
+  slope_base_vars <- c("usa-facts_confirmed_incidence_num_lag",
+                       "fb-survey_smoothed_hh_cmnty_cli_lag",
+                       "indicator-combination_nmf_day_doc_fbc_fbs_ght_lag")
+  new_train_X <- matrix(NA, nrow=nrow(train_X), ncol=length(slope_base_vars))
+  new_test_X <-  matrix(NA, nrow=nrow(test_X), ncol=length(slope_base_vars))
+  for (i in 1:length(slope_base_vars)) {
+    var <- slope_base_vars[i]
+    n_chars <- nchar(var)
+    var_cols <- which(substring(colnames(train_X), 1, n_chars)==var)
+    n_var_cols <- length(var_cols)
+    if (n_var_cols <= 1) {
+      stop("Could not create slope vars in training X, not enough columns for ", var)
+    }
+    new_train_X[,i] <- stats::lsfit(-(1:n_var_cols), t(train_X[, var_cols]))$coef[2,]
+    new_test_X[,i] <- stats::lsfit(-(1:n_var_cols), t(test_X[, var_cols]))$coef[2,]
+  }
+  
+  # add first lags of handpicked variables
+  first_case_var <- which(grepl("usa-facts_confirmed_incidence_num_lag", colnames(train_X)))[1]
+  first_fb_var <- which(grepl("fb-survey_smoothed_hh_cmnty_cli_lag", colnames(train_X)))[1]
+  first_ind_var <- which(grepl("indicator-combination_nmf_day_doc_fbc_fbs_ght_lag", colnames(train_X)))[1]
+  pop_var <- which(grepl("population", colnames(train_X)))[1]
+  new_train_X <- cbind(new_train_X,
+                       train_X[, first_case_var],
+                       train_X[, first_fb_var],
+                       train_X[, first_ind_var],
+                       train_X[, pop_var])
+  new_test_X <- cbind(new_test_X,
+                      test_X[, first_case_var],
+                      test_X[, first_fb_var],
+                      test_X[, first_ind_var],
+                      test_X[, pop_var])
+  
+  covariate_names <- c(paste(slope_base_vars, "slope", sep="_"),
+                       colnames(train_X)[first_case_var],
+                       colnames(train_X)[first_fb_var],
+                       colnames(train_X)[first_ind_var],
+                       colnames(train_X)[pop_var])
+  
+  colnames(new_train_X) <- covariate_names
+  colnames(new_test_X) <- covariate_names
+
+  # add interaction terms
+  if (modeling_options$add_interactions) {
+    X_names <- colnames(train_X)
+    new_X_names <- colnames(new_train_X)
+    
+    fb_slope_var <- lubridate::intersect(
+      which(grepl("fb-survey_smoothed_hh_cmnty_cli_lag", new_X_names)),
+      which(grepl("slope", new_X_names)))
+    ind_slope_var <- lubridate::intersect(
+      which(grepl("indicator-combination_nmf_day_doc_fbc_fbs_ght_lag", new_X_names)),
+      which(grepl("slope", new_X_names)))
+    
+    new_train_X <- cbind(new_train_X,
+                         train_X[, first_case_var]*train_X[, first_fb_var],
+                         train_X[, first_case_var]*train_X[, first_ind_var],
+                         train_X[, first_case_var]*new_train_X[, fb_slope_var],
+                         train_X[, first_case_var]*new_train_X[, ind_slope_var])
+    new_test_X <- cbind(new_test_X,
+                        test_X[, first_case_var]*test_X[, first_fb_var],
+                        test_X[, first_case_var]*test_X[, first_ind_var],
+                        test_X[, first_case_var]*new_test_X[, fb_slope_var],
+                        test_X[, first_case_var]*new_test_X[, ind_slope_var])
+    
+    covariate_names <- c(new_X_names, 
+                         paste(X_names[first_case_var],
+                               X_names[c(first_fb_var, first_ind_var)], sep="*"),
+                         paste(X_names[first_case_var],
+                               new_X_names[c(fb_slope_var, ind_slope_var)], sep="*"))
+    colnames(new_train_X) <- covariate_names
+    colnames(new_test_X) <- covariate_names
+  }
+  
+  logger::log_debug("Input colnames:\n",
+                    paste(colnames(new_train_X), collapse='\n'))
+  
+  # finally, get predictions
+  out <- matrix(NA, nrow=nrow(test_X), ncol=length(modeling_options$cdc_probs))
+  quantile_fits <- list()
+  ls_fits <- list()
+  for (i in 1:modeling_options$n_clusters) {
+    logger::log_debug(paste("Fitting cluster", i))
+    locs_in_cluster <- cluster_locs %>% 
+      filter(cluster_id == i) %>%
+      pull(geo_value)
+    train_idx <- which(train_row_locations %in% locs_in_cluster)
+    test_idx <- which(test_row_locations %in% locs_in_cluster)
+    
+    # get quantiles
+    if (modeling_options$use_cv_lasso) {
+      n_train_cluster <- nrow(new_train_X[train_idx,]) 
+      n_cv <- floor(0.5 * n_train_cluster) + 1
+      cv_inds <- c(n_cv:n_train_cluster)
+      cv_list <- list(train = list(cv_inds), 
+                      test = list(setdiff(1:n_train_cluster, cv_inds)))
+
+      cluster_cv_fit_quantiles <- quantgen::cv_quantile_lasso(new_train_X[train_idx,],
+                                                             train_y[train_idx,],
+                                                             tau = modeling_options$fitting_tau,
+                                                             standardize = TRUE,
+                                                             nfolds = 1,
+                                                             train_test_inds = cv_list,
+                                                             verbose = TRUE,
+                                                             lp_solver = modeling_options$lp_solver)
+      
+      cluster_fit_quantiles <- quantgen::refit_quantile_lasso(
+        cluster_cv_fit_quantiles,
+        new_train_X[train_idx,],
+        train_y[train_idx,],
+        tau = modeling_options$fitting_tau,
+        standardize = TRUE)
+      preds <-
+        quantgen::quantile_extrapolate(modeling_options$fitting_tau,
+                                       predict(cluster_fit_quantiles, 
+                                               new_test_X[test_idx, ],
+                                               sort = TRUE),
+                                       tau_out = modeling_options$cdc_probs)
+      
+    } else {
+      cluster_fit_quantiles <- quantgen::quantile_lasso(new_train_X[train_idx,],
+                                                        train_y[train_idx,],
+                                                        tau = modeling_options$cdc_probs,
+                                                        standardize = FALSE,
+                                                        lambda = 0)
+      preds <-  predict(cluster_fit_quantiles, new_test_X[test_idx, ], sort = TRUE)
+    }
+    colnames(preds) <- modeling_options$cdc_probs
+    
+    # maybe replace center point estimate with LS fit
+    cluster_fit_ls <- NULL
+    if (!modeling_options$use_median_point) {
+      logger::log_debug("Replacing median estimate with LS point estimate")
+      cluster_fit_ls <- glmnet::glmnet(new_train_X[train_idx,],
+                                    train_y[train_idx,],
+                                    standardize=FALSE,
+                                    alpha=0, # 0=ridge, 1=lasso
+                                    lambda=0)
+      
+      preds[, colnames(preds) == "0.5"] <- predict(cluster_fit_ls, new_test_X[test_idx,])
+    }
+    
+    # save fits for debug
+    quantile_fits[[i]] <- cluster_fit_quantiles
+    ls_fits[[i]] <- cluster_fit_ls
+    
+    out[test_idx, ] <- preds
+  }
+
+  # output debug objects
+  if (!is.null(modeling_options$debug_clusters_folder)) {
+    dir.create(file.path(modeling_options$debug_clusters_folder))
+    out_file <- file.path(
+      modeling_options$debug_clusters_folder,
+      paste0("debug_", modeling_options$forecast_date,
+             "_ahead_", modeling_options$ahead, ".RDS")
+    )
+    out_obj <- list(
+        modeling_options = modeling_options,
+        train_test = train_test,
+        cluster_locs = cluster_locs,
+        ls_fits = ls_fits,
+        quantile_fits = quantile_fits,
+        train_X = new_train_X,
+        train_y = train_y,
+        test_X = new_test_X
+      )
+    saveRDS(out_obj, out_file)
+    logger::log_debug(paste0("Saved ", out_file))
+  }
+  
+  # isotonic regression to ensure quantiles ordering
+  for (i in 1:nrow(out)) {
+    out[i, ] <- Iso::pava(out[i, ])
+  }
+  
+  # transform back to correct scale for clipping
+  if (modeling_options$log_response) {
+    out <- tr.inv_log_pad(out)
+  }
+  
+  # ensure negatives are set to 0
+  out[out < 0] <- 0
+  
+  # format for output
+  final_out <- NULL
+  for (i in 1:length(modeling_options$cdc_probs)) {
+    final_out <- rbind(final_out, cbind(train_test$test_row_locations, 
+                                        modeling_options$cdc_probs[i],
+                                        out[, i]))
+  }
+  names(final_out) <- c("location", "probs", "quantiles")
+  
+  final_out
+}
+
