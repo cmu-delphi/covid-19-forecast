ca7ffd010834028c18e600a43c57c2aad98aa406
yelselmiao <lesleytam97@gmail.com>
2021-01-10 21:07:50 -0800

    Update correctors.county.R

--- utilities/zookeeper/R/correctors.county.R
+++ utilities/zookeeper/R/correctors.county.R
@@ -80,11 +80,15 @@ zyzzyva_county_corrections <- function(signals_list, ...){
   }
   
   # Multiple Signals
-  if (params$signals_to_correct == c("deaths_incidence_num","confirmed_incidence_num")) {
+  if (all(params$signals_to_correct == c("deaths_incidence_num","confirmed_incidence_num"))|
+      all(params$signals_to_correct == c("confirmed_incidence_num","deaths_incidence_num"))) {
+    
     corrected <- list()
     for (i in seq_along(signals_list)) {
       corrected[[i]] <- zyzzyva_county_corrections_single_signal(
         signals_list[[i]], params)
+    }
+    for (i in seq_along(signals_list)) {
       signals_list[[i]] <- corrected[[i]] %>%
         mutate(value = .data$corrected) %>%
         select(all_of(in_names))
@@ -104,56 +108,62 @@ zyzzyva_county_corrections <- function(signals_list, ...){
 
 
 zyzzyva_county_corrections_single_signal <- function(x, params) {
-  if (unique(x$signal) == "confirmed_incidence_num") {
-    x <- x %>% group_by(geo_value) %>% 
-      dplyr::mutate(
-        fmean = roll_meanr(.data$value, params$window_size),
-        fmedian = roll_medianr(.data$value, params$window_size),
-        smedian = roll_median(.data$value, params$window_size, fill = NA),
-        fsd = roll_sdr(.data$value, params$window_size),
-        ssd = roll_sd(.data$value, params$window_size, fill = NA),
-        fmad = roll_medianr(abs(.data$value-.data$fmedian), params$window_size,na.rm=TRUE),
-        smad = roll_median(abs(.data$value-.data$smedian), na.rm=TRUE),
-        ftstat = abs(.data$value - .data$fmedian) / .data$fsd,
-        ststat = abs(.data$value - .data$smedian) / .data$ssd,
-        flag = 
-          (abs(.data$value) > params$size_cut & 
-             !is.na(.data$ststat) & 
-             .data$ststat > params$sig_cut ) | # best case
-          (is.na(.data$ststat) & abs(.data$value) > params$size_cut & 
+  x <- x %>% group_by(geo_value) %>% 
+    dplyr::mutate(
+      fmean = roll_meanr(.data$value, params$window_size),
+      fmedian = roll_medianr(.data$value, params$window_size),
+      smedian = roll_median(.data$value, params$window_size, fill = NA),
+      fsd = roll_sdr(.data$value, params$window_size),
+      ssd = roll_sd(.data$value, params$window_size, fill = NA),
+      fmad = roll_medianr(abs(.data$value-.data$fmedian), params$window_size,na.rm=TRUE),
+      smad = roll_median(abs(.data$value-.data$smedian), na.rm=TRUE),
+      ftstat = abs(.data$value - .data$fmedian) / .data$fsd,
+      ststat = abs(.data$value - .data$smedian) / .data$ssd,
+      excess = .data$value - na_replace(.data$smedian, .data$fmedian),
+      excess = floor(.data$excess - params$excess_cut*sign(.data$excess)*na_replace(.data$smad,.data$fmad)),
+      flag = 
+        (abs(.data$value) > params$size_cut & 
+            !is.na(.data$ststat) & 
+            .data$ststat > params$sig_cut ) | # best case
+        (is.na(.data$ststat) & abs(.data$value) > params$size_cut & 
              !is.na(.data$ftstat) & .data$ftstat > params$sig_cut) | 
           # use filter if smoother is missing
-          (.data$value < -params$size_cut & 
+        (.data$value < -params$size_cut & 
              !is.na(.data$ststat) & !is.na(.data$ftstat)), # big negative
-        flag = .data$flag | # these allow smaller values to also be outliers if they are consecutive
-          (dplyr::lead(.data$flag) & !is.na(.data$ststat) & .data$ststat > params$sig_consec) | 
-          (dplyr::lag(.data$flag) & !is.na(.data$ststat) & .data$ststat > params$sig_consec) |
-          (dplyr::lead(.data$flag) & is.na(.data$ststat) & .data$ftstat > params$sig_consec) |
-          (dplyr::lag(.data$flag) & is.na(.data$ststat) & .data$ftstat > params$sig_consec),
-        flag = .data$flag & 
-          (.data$time_value < ymd(params$time_value_flag_date) | 
-             .data$value < -params$size_cut),
-        flag = .data$flag | 
-          (.data$time_value == "2020-11-20" & as.numeric(.data$geo_value) %/% 1000 == 22),
+      flag = .data$flag | # these allow smaller values to also be outliers if they are consecutive
+        (dplyr::lead(.data$flag) & !is.na(.data$ststat) & .data$ststat > params$sig_consec) | 
+        (dplyr::lag(.data$flag) & !is.na(.data$ststat) & .data$ststat > params$sig_consec) |
+        (dplyr::lead(.data$flag) & is.na(.data$ststat) & .data$ftstat > params$sig_consec) |
+        (dplyr::lag(.data$flag) & is.na(.data$ststat) & .data$ftstat > params$sig_consec),
+      flag = .data$flag & 
+        (.data$time_value < ymd(params$time_value_flag_date) | 
+            .data$value < -params$size_cut),
+      flag = .data$flag | 
+        (.data$time_value == "2020-11-20" & as.numeric(.data$geo_value) %/% 1000 == 22),
         #Louisiana backlog drop https://ldh.la.gov/index.cfm/newsroom/detail/5891
-        flag_bad_RI = ((substr(.data$geo_value,start = 1, stop = 2) == "44") &
+      flag_bad_RI = ((substr(.data$geo_value,start = 1, stop = 2) == "44") &
                          .data$value > 10 &
-                         abs(lag(.data$value) < params$integer_tol)),
-        excess = .data$value - na_replace(.data$smedian, .data$fmedian),
-        excess = floor(.data$excess - params$excess_cut*sign(.data$excess)*na_replace(.data$smad,.data$fmad)),
-        corrected = corrections_multinom_roll( # fix RI reporting problem
-          .data$value, .data$value, .data$flag_bad_RI, .data$time_value, 7),
-        corrected = corrections_multinom_roll( # for everywhere else
-          .data$corrected, .data$excess, (.data$flag & !.data$flag_bad_RI ),
-          .data$time_value, params$backfill_lag,
-          reweight=function(x) exp_w(x, params$backfill_lag))
+                         abs(lag(.data$value) < params$integer_tol))
       )
+        
+  if (unique(x$signal) == "confirmed_incidence_num") {
+    x <- x %>% mutate(corrected = corrections_multinom_roll( # fix RI reporting problem
+                        .data$value, .data$value, .data$flag_bad_RI, .data$time_value, 7),
+                      corrected = corrections_multinom_roll( # for everywhere else
+                        .data$corrected, .data$excess, (.data$flag & !.data$flag_bad_RI ),
+                        .data$time_value, params$backfill_lag,
+                        reweight=function(x) exp_w(x, params$backfill_lag)))
   }
-  
+   
   
   if (params$multinomial_preprocessor) {
-    x <- x %>% mutate(corrected = multinomial_roll_sum(.data$corrected))
+    if (unique(x$signal) == "confirmed_incidence_num") {
+      x <- x %>% mutate(corrected = multinomial_roll_sum(.data$corrected))
+    }else{
+      x <- x %>% mutate(corrected = multinomial_roll_sum(.data$value))
+    }
   }
+  
   x <- x %>% mutate(corrected = .data$corrected + missing_future(substr(.data$geo_value,start = 1, stop = 2) == "44", 
                                                                  .data$time_value,
                                                                  .data$excess,.data$fmean))
