a34c35384867325840426c6155dcd87776ebeb37
capolitsch <capolitsch@cmu.edu>
2021-02-11 12:57:12 -0500

    Debug - still getting geo_type are when running

--- forecasters/aardvark/R/modeling.R
+++ forecasters/aardvark/R/modeling.R
@@ -1,7 +1,6 @@
-make_aardvark_forecaster <- function(geo_type = NULL, response = NULL, features = NULL,
-                                     bandwidth = 7, degree = 0, smoother = NULL, 
-                                     stratifier = NULL, modeler = NULL,aligner = NULL, 
-                                     bootstrapper, B = 1000){
+make_aardvark_forecaster <- function(response = NULL, features = NULL, bandwidth = 7, 
+                                     degree = 0, smoother = NULL, stratifier = NULL, 
+                                     modeler = NULL,aligner = NULL, bootstrapper, B = 1000){
   
   covidhub_probs <- c(0.01, 0.025, seq(0.05, 0.95, by = 0.05), 0.975, 0.99)
   
