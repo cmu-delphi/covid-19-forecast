3cbccdae9ca956cd5cb4dd6413f0efeba453a078
Daniel J. McDonald <dajmcdon@gmail.com>
2021-04-20 18:03:06 -0700

    allow for county filtering to have a window of arbitrary size rather than all available data

--- forecasters/animalia/R/featurizers.R
+++ forecasters/animalia/R/featurizers.R
@@ -8,6 +8,11 @@
 #' @param response_data_source the name of the response
 #' @param response_signal the name of the response signal
 #' @param n_locations the number of locations to predict
+#' @param location_size_filter_lookback when summing county totals, how many
+#'   days back should we sum over? Defaults to all available. If you specify
+#'   this argument, you must also pass the forecast date.
+#' @param forecast_date required for non null `lovation_size_filter_lookback`
+#'   otherwise ignored
 #' 
 #'
 #'
@@ -38,11 +43,31 @@ make_state_7dav_featurizer <- function() {
 #' @export
 make_county_7dav_featurizer <- function(response_data_source = "jhu-csse",
                                         response_signal = "confirmed_incidence_num",
-                                        n_locations = 200) {
+                                        n_locations = 200,
+                                        location_size_filter_lookback = NULL,
+                                        forecast_date = NULL) {
   response_name = rlang::sym(
     paste0("value+0:", response_data_source, "_", response_signal))
+  if (!is.null(location_size_filter_lookback)) 
+    assertthat::assert_that(
+      !is.null(forecast_date),
+      msg = paste0("When filtering counties for specific recent period with\n",
+                   "a specified `size_filter_lookback`, `forecast_date`\n",
+                   "must be present."))
   function(df) {
+    first_day_available <- min(df$time_value)
+    window_start_day <- lubridate::as_date(ifelse(
+      is.null(location_size_filter_lookback),
+      first_day_available,
+      forecast_date - 
+        lubridate::duration(location_size_filter_lookback, "days")))
+    if (window_start_day < first_day_available) {
+      warning("You're trying to use a larger window to choose\n",
+              "than you have data available. Using everything.")
+      window_start_day <- first_day_available
+    }
     locs <- df %>% 
+      filter(.data$time_value >= window_start_day) %>%
       group_by(.data$geo_value) %>%
       summarise(tot = sum(!!response_name, na.rm = TRUE)) %>%
       slice_max(order_by = .data$tot, n = n_locations) %>%
