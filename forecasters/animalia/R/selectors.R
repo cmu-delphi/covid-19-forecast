9940ee793a31779c57e0cf5deb583e887cd320fc
Kenneth Tay <kjytay@gmail.com>
2021-05-05 15:36:44 -0700

    Fix selectors.R so that the functions it creates takes in df in list format instead of wide format.

--- forecasters/animalia/R/selectors.R
+++ forecasters/animalia/R/selectors.R
@@ -1,31 +1,29 @@
-#' Functions to create "selectors" 
+#' Function factory for creating "selector" functions based on top response 
+#' count
 #' 
-#' These create functions that take in a dataframe in wide format and
-#' return a vector of geo_values. These geo_values are the ones that we
+#' This creates a function that takes in a dataframe in list format and
+#' returns a vector of geo_values. These geo_values are the ones that have the
+#' largest cumulative value of the response, and are the ones that we
 #' want forecasts for. (We currently only use them for county case 
 #' predictions.)
 #' 
-#' @param response_data_source The name of the response.
-#' @param response_signal The name of the response signal.
-#' @param n_locations The number of locations to predict.
+#' @param n_locations The number of locations to predict/return.
 #' 
-#' @return A function that takes in a data frame in wide format and returns
+#' @return A function that takes in a data frame in list format and returns
 #'   a vector of geo_values.
 #' @name selectors
 #' @importFrom dplyr group_by slice_max summarise
 #' @importFrom rlang .data
 #' @export
-select_geo_top_n <- function(response_data_source = "jhu-csse",
-                             response_signal = "confirmed_incidence_num",
-                             n_locations = 200) {
+select_geo_top_n <- function(n_locations = 200) {
   if (n_locations <= 0) stop("n_locations must be a positive integer")
-  response_name = rlang::sym(
-    paste0("value+0:", response_data_source, "_", response_signal))
   
-  function(df) {
-    geo_values <- df %>% 
+  function(df_list) {
+    if (class(df_list)[1] != "list") df_list <- list(df_list)
+    
+    geo_values <- df_list[[1]] %>% 
       group_by(.data$geo_value) %>%
-      summarise(tot = sum(!!response_name, na.rm = TRUE)) %>%
+      summarise(tot = sum(value, na.rm = TRUE)) %>%
       slice_max(order_by = .data$tot, n = n_locations) %>%
       select(.data$geo_value) %>%
       pull()
--- forecasters/animalia/tests/testthat/test-selectors.R
+++ forecasters/animalia/tests/testthat/test-selectors.R
@@ -1,27 +1,27 @@
 df_county_wide <- tibble::tibble(
+  data_source = "jhu-csse",
+  signal = "confirmed_incidence_num",
   geo_value = rep(c("00001", "00002", "00003", "00004", "00005"), each = 3),
   time_value = rep(as.Date("2021-01-01") + 0:2, times = 5),
-  `value+0:jhu-csse_confirmed_incidence_num` = 1:15,
-  `value-7:jhu-csse_confirmed_incidence_num` = 15:1,
-  `value+7:jhu-csse_confirmed_incidence_num` = c(1:10, 1:5)
+  value = 1:15
 )
 
-df_state_wide <- tibble::tibble(
-  geo_value = rep(c("ak", "ca", "fl", "tx", "wa"), each = 3),
-  time_value = rep(as.Date("2021-01-01") + 0:2, times = 5),
-  `value+0:jhu-csse_deaths_incidence_num` = 1:15,
-  `value-7:jhu-csse_deaths_incidence_num` = 15:1,
-  `value+7:jhu-csse_deaths_incidence_num` = c(1:10, 1:5)
+df_state_wide <- list(
+  tibble::tibble(
+    data_source = "jhu-csse",
+    signal = "csse_deaths_incidence_num",
+    geo_value = rep(c("ak", "ca", "fl", "tx", "wa"), each = 3),
+    time_value = rep(as.Date("2021-01-01") + 0:2, times = 5),
+    value = c(1:3, 1:3, 2:4, c(2, 3, 5), c(6, 4, 1))
+  ),
+  tibble::tibble(
+    data_source = "fb-survey",
+    signal = "smoothed_cli",
+    geo_value = rep(c("ak", "ca", "fl", "tx", "wa"), each = 3),
+    time_value = rep(as.Date("2021-01-01") + 0:2, times = 5),
+    value = 15:1
+  )
 )
-
-df_wide <- tibble::tibble(
-  geo_value = rep(c("00001", "00002", "00003", "00004", "00005"), each = 3),
-  time_value = rep(as.Date("2021-01-01") + 0:2, times = 5),
-  `value+0:fb-survey_smoothed_cli` = 1:15,
-  `value-7:fb-survey_smoothed_cli` = 15:1,
-  `value+7:fb-survey_smoothed_cli` = c(1:10, 1:5)
-)
-
 
 test_that("select_geo_top_n, filter not active", {
   selection_fn <- select_geo_top_n()
@@ -35,19 +35,10 @@ test_that("select_geo_top_n, filter is active", {
                c("00005", "00004"))
 })
 
-test_that("select_geo_top_n, different response signal", {
-  selection_fn <- select_geo_top_n(response_signal = "deaths_incidence_num",
-                                     n_locations = 3)
+test_that("select_geo_top_n, filter is active, list provided", {
+  selection_fn <- select_geo_top_n(n_locations = 3)
   expect_equal(selection_fn(df_state_wide),
                c("wa", "tx", "fl"))
-})
-
-test_that("select_geo_top_n, different response data source", {
-  selection_fn <- select_geo_top_n(response_data_source = "fb-survey",
-                                   response_signal = "smoothed_cli",
-                                   n_locations = 1)
-  expect_equal(selection_fn(df_wide),
-               c("00005"))
 })
 
 test_that("select_geo_top_n, invalid n_locations", {
